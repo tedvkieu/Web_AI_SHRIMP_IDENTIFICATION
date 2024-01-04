2e82d777fbe1c61f5d21c682e577b1df4c7c1039
kieuishere <dvkieu165@gmail.com>
2024-01-04 14:58:29 +0700

    upload interface

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, redirect, url_for,render_template
+from flask import Flask, redirect, url_for,render_template, send_from_directory,request, jsonify
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
 import os 
 from wtforms.validators import InputRequired
+import requests
+from io import BytesIO
+from PIL import Image
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
@@ -13,14 +17,22 @@ class uploadFileForm(FlaskForm):
     file = FileField("File", validators = [InputRequired()])
     submit = SubmitField("Upload File")
 
+@app.route('/uploads/<filename>')
+def get_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],filename) 
+
 @app.route('/', methods = ['GET', 'POST'])
 def index():
    form = uploadFileForm()
    if form.validate_on_submit():
        file = form.file.data
+       
+       filename = secure_filename(file.filename)
+       file_url = url_for('get_file', filename = filename)
        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
-       return "File has been uploaded"
-   return render_template("index.html", form = form )
+   else:
+        file_url = None    
+   return render_template("index.html", form = form, file_url = file_url)
 
 @app.route('/user/<name>')
 def hello_user(name):
