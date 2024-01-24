dcbcfee38c00f862db1729dc531c964fced76436
kieuishere <dvkieu165@gmail.com>
2024-01-24 08:39:31 +0700

    test login

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, redirect, url_for,render_template, send_from_directory,request, send_file,jsonify
+from flask import Flask, redirect, url_for,render_template, send_from_directory,request, send_file,jsonify, session
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
+from flask_sqlalchemy import SQLAlchemy
+from enum import EnumMeta
+from os import path
 import os 
+from datetime import timedelta
+from flask.helpers import flash
 from wtforms.validators import InputRequired
 import requests
 from io import BytesIO
@@ -12,8 +17,71 @@ from urllib.parse import urlparse
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['UPLOAD_FOLDER'] = 'static/images/img_process'
+app.permanent_session_lifetime = timedelta(minutes=1)
 
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+@app.route('/login', methods =["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user_name = request.form["name"]
+        session.permanent=True
+        if user_name:
+            session["user"] = user_name
+            found_user = User.query.filter_by(name = user_name).first()
+            if found_user:
+                session["email"] = found_user.email
+            else:   
+                user = User(user_name, 'temp@gmail.com')
+                db.session.add(user)
+                db.session.commit()
+            flash("created in DB!")
+            flash("you logged in successfully!", "info")
+            return redirect(url_for("user", user = user_name))
+        if "user" in session:
+            name = session["user"]
+            flash("you have already logged in!", "info")
+            return redirect(url_for("user", user = user_name))
+    return render_template("login.html")
+
+@app.route('/user', methods = ["POST", "GET"])
+def user():
+    email = None
+
+    if "user" in session:
+       name = session["user"]
+       if request.method == "POST":
+           if not request.form["email"] and request.form["name"]:
+               User.query.filter_by(name = name).delete()
+               db.session.commit()
+               flash("deleted user!")
+               return redirect(url_for("log_out"))
+           else:
+                email = request.form["email"]
+                session["email"] = email
+                found_user = User.query.filter_by(name = name).first()
+                found_user.email = email
+                db.session.commit()
+                flash("email updated")
+       elif "email" in session:
+           email = session["email"]
+       return render_template("user.html", user = name, email = email)
+    else:
+        flash("you haven't logged in", "info")
+        return redirect(url_for("login"))
 
 class uploadFileForm(FlaskForm):
     file = FileField("File", validators = [InputRequired()])
@@ -75,4 +143,8 @@ def download():
         return jsonify({'success': False, 'error': str(e)})
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    with app.app_context():
+        if not path.exists("user.db"):
+            db.create_all()
+            print("Created database!")
+        app.run(debug=True)
